ada4b8a1279649cf194f664b0a8318dadb960968
mitzCanCode <dimitris.chatzi@proton.me>
2025-07-29 10:26:52 +0300

    Code for lab5.js completed and removed redundant forgoten code from lab4.js

--- module2/lab5.js
+++ module2/lab5.js
@@ -53,14 +53,13 @@ class User {
 
   sendMessage(from, message) {
     sendEmail({
-      from: from,
-      to: this,
+      from: this,
+      to: from,
       message: message,
     });
   }
 
   showMessagesHistory() {
-    console.log(this.messages);
     for (let message of this.messages) {
       console.log(`${message.from.email} -> ${this.email} ${message.message}`);
     }
@@ -207,16 +206,32 @@ class Tutoring {
   }
 }
 
-let tutoring = new Tutoring();
+class ExtendedTutoring extends Tutoring {
+  constructor() {
+    super();
+  }
+
+  sendMessages(from, to, message) {
+    for (let recipient of to) {
+      from.sendMessage(recipient, message);
+    }
+  }
+}
+
+let tutoring = new ExtendedTutoring();
 tutoring.addStudent("Rafael", "Fife", "rfife@rhyta.com");
 tutoring.addStudent("Kelly", "Estes", "k_estes@dayrep.com");
 tutoring.addTeacher("Paula", "Thompkins", "PaulaThompkins@jourrapide.com");
-let student = tutoring.getStudentByName("Rafael", "Fife");
-student.addCourse("maths", 2);
-student.addCourse("physics", 4);
-let teacher = tutoring.getTeacherByName("Paula", "Thompkins");
-teacher.addCourse("maths", 4);
-let students = tutoring.getTeacherForStudent(student);
-let teachers = tutoring.getStudentsForTeacher(teacher);
-console.log(students[0]); // -> Teacher {name: 'Paula', surname: 'Thompkins', ...
-console.log(teachers[0]); // -> Student {name: 'Rafael', surname: 'Fife', ...
+let to = [];
+to.push(tutoring.getStudentByName("Rafael", "Fife"));
+to.push(tutoring.getStudentByName("Kelly", "Estes"));
+tutoring.sendMessages(
+  tutoring.getTeacherByName("Paula", "Thompkins"),
+  to,
+  "test message",
+);
+for (let user of to) {
+  user.showMessagesHistory();
+}
+// -> PaulaThompkins@jourrapide.com -> rfife@rhyta.com: test message
+// -> PaulaThompkins@jourrapide.com -> k_estes@dayrep.com: test message
